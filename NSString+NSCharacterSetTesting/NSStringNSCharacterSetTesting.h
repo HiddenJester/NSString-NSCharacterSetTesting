51e92fbd424a733b4bdb9eb1188afcc7297cfa84
Timothy Sanders <tsanders@hiddenjester.com>
2016-06-25 08:07:26 -0700

    Setting up a framework for Carthage.

---
+++ NSString+NSCharacterSetTesting/NSStringNSCharacterSetTesting.h
@@ -0,0 +1,16 @@
+//
+//  NSStringNSCharacterSetTesting.h
+//  NSStringNSCharacterSetTesting
+//
+//  Created by Timothy Sanders on 6/25/16.
+//  Copyright © 2016 Timothy Sanders. All rights reserved.
+//
+
+//! Project version number for NSString+NSCharacterSetTesting.
+FOUNDATION_EXPORT double NSString_NSCharacterSetTestingVersionNumber;
+
+//! Project version string for NSString+NSCharacterSetTesting.
+FOUNDATION_EXPORT const unsigned char NSString_NSCharacterSetTestingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WatchColorsiOS/PublicHeader.h>
+#import <NSStringNSCharacterSetTesting/NSString+NSCharacterSetTesting.h>
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 NSString-NSCharacterSetTesting
 ==============================
 
+[![Carthage compatible](https://img.shields.io/badge/Carthage-compatible-4BC51D.svg?style=flat)](https://github.com/Carthage/Carthage)
+
 A category on NSString to test whether all characters in the string are members of a NSCharacterSet. For example, if you want
 to determine if a string is entirely composed of numbers and a decimal point this function can do that. If you have a text
 field that should only accept lowercase letters then this method allows easy testing of the input.
