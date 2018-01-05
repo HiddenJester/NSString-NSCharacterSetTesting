6fadee66ce825a2335bb66f91b1b066514532aeb
Timothy Sanders <tsanders@hiddenjester.com>
2018-01-04 17:37:57 -0800

    Updated the license, but I missed the comments.

--- NSString+NSCharacterSetTesting/NSString+NSCharacterSetTesting.h
+++ NSString+NSCharacterSetTesting/NSString+NSCharacterSetTesting.h
@@ -3,8 +3,8 @@
 //
 //  Created by Timothy Sanders on 2/17/14.
 //  Copyright (c) 2014 HiddenJester Software.
-//	This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
-//	See http://creativecommons.org/licenses/by-nc-sa/4.0/
+//	This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
+//	See http://creativecommons.org/licenses/by-sa/4.0/
 
 @import Foundation;
 
--- NSString+NSCharacterSetTesting/NSString+NSCharacterSetTesting.m
+++ NSString+NSCharacterSetTesting/NSString+NSCharacterSetTesting.m
@@ -3,9 +3,6 @@
 //  RoadTrip
 //
 //  Created by Timothy Sanders on 2/17/14.
-//  Copyright (c) 2014 HiddenJester Software.
-//	This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
-//	See http://creativecommons.org/licenses/by-nc-sa/4.0/
 
 #import "NSString+NSCharacterSetTesting.h"
 
